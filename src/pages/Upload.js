cd8d7d1da65b65998b219dce2d14a900036b1317
Aswin <aswinapxyz@cet.ac.in>
2022-12-11 01:51:21 +0530

    upload page

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './css/upload.css'
 
 
-function Upload() {
+function Upload(props) {
+  const setVisible=props.setVisible;
+    const visible=props.visible;
+
   return (
       <div className='upload'>
-        <p>Upload File</p>
+        <p>Upload Your File</p>
         <img src={require('../assets/removed.png')} alt='bg' />
-        <input type="file" value="" />
+        <input className='input' type="file" value="" />
+        <input className='upbtn' type="button" value='Submit'/>
+        <button onClick={() => setVisible(!visible)} className='close'>Close</button>
       </div>
   )
 }
