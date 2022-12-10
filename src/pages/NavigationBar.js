8790509d82310579a4a7d41bb90c11b5d86f3802
Fausan Asharaf <fausan.asharaf01@gmail.com>
2022-12-10 10:43:43 +0000

    testcommit

--- src/pages/NavigationBar.js
+++ src/pages/NavigationBar.js
@@ -5,7 +5,7 @@ import './css/Navbar.css'
 function NavigationBar() {
   return (
     <div className='navbar'>
-        <div className='logo'><img src={require("../assets/logo.png")}></img></div>
+        <div className='logo'><img alt='bg' src={require("../assets/logo.png")}></img></div>
         <div className='list'>
             <Link className='link' to="/">Home</Link>
             <Link className='link' to="/About">About</Link>
