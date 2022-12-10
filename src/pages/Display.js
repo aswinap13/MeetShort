ec73a5915769da3a5bf9456b80bd5b09604ddfc2
Aswin <aswinapxyz@cet.ac.in>
2022-12-11 02:27:34 +0530

    display

---
+++ src/pages/Display.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './css/display.css'
+
+function Display() {
+  return (
+    <div className='display'>
+        <h3>Summary</h3>
+        <div className='asr'>
+            <div >
+                <h4>Speaker 1</h4>
+            </div>
+            <div >
+                <h4>Speaker 1</h4>
+            </div>
+            <div >
+                <h4>Speaker 1</h4>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Display
