51dd0b6fa50d2e3d844d39bbd516e7253a5e91f4
Aswin <aswinapxyz@cet.ac.in>
2022-12-11 13:59:36 +0530

    h

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -6,11 +6,13 @@ function Upload(props) {
   const setVisible=props.setVisible;
     const visible=props.visible;
 
+    
+
   return (
       <div className='upload'>
         <p>Upload Your File</p>
         <img src={require('../assets/removed.png')} alt='bg' />
-        <input className='input' type="file" value="" />
+        <input className='input' type="file" value=""/>
         <input className='upbtn' type="button" value='Submit'/>
         <button onClick={() => setVisible(!visible)} className='close'>Close</button>
       </div>
